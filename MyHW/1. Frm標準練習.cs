921a8541e21f9882f5e5b7da979ce566d09bf5e5
政宏 林 <hon77514@gmail.com>
2022-04-12 13:40:16 +0800

    加入專案檔案。

---
+++ MyHW/1. Frm標準練習.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FrmHomeWork : Form
+    {
+        public FrmHomeWork()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 100;
+            int b = 66;
+            int c = 77;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int[] nums = { 33, 4, 5, 11, 222, 34 };
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            string[] names = { "aaa", "ksdkfjsdk"};
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int[] scores = { 2, 3, 46, 33, 22, 100,150, 33,55};
+
+            int max =  scores.Max();
+            //MessageBox.Show("Max = " + max);
+
+            //Array.Sort(scores);
+            //MessageBox.Show("Max =" + scores[scores.Length - 1]);
+
+            //================================
+
+            //Point[] points = new Point[3];
+            //points[0].X = 3;
+            //points[0].Y = 4;
+            ////System.InvalidOperationException: '無法比較陣列中的兩個元素。'
+
+            //Array.Sort(points);
+
+            //=================================
+
+            
+        }
+
+        int MyMinScore(int[] nums)
+        {
+            return 10;
+        }
+    }
+}
